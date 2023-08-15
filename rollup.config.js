d97ba55e83f4ee71674f3af563e3d4fffb11183c
Victor Casta <victorarroyavecastaneda@gmail.com>
2023-08-15 12:05:05 -0500

    init configure de project used rollup for generic html files

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import html from '@rollup/plugin-html';
+import copy from 'rollup-plugin-copy';
+import image from '@rollup/plugin-image';
+import css from 'rollup-plugin-css-only';
+
+
+export default {
+    input: "src/index.js",
+    output: {
+        file: "dist/bundle.js",
+        format: "es",
+        sourcemap: true,
+    },
+    plugins: [
+        copy({
+            targets: [
+                { src: "src/assets/**/*", dest: "dist/asset" },
+            ]
+        }),
+        css({
+            output: 'bundle.css'
+        }),
+        html(),
+        image(),
+    ]
+}
